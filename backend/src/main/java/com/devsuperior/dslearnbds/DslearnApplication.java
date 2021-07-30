97226fd0b71d70ba9b5e1702bfb05389386de15a
Danielle Balogh <daniellegb@protonmail.com>
2021-07-30 18:04:02 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/DslearnApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnApplication.class, args);
+	}
+
+}
