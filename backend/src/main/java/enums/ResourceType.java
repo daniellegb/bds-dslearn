23c5e78a7d64dfb896f5126c491ccd4d75b3bd79
Danielle Balogh <daniellegb@protonmail.com>
2021-08-12 19:47:25 -0300

    Resource

---
+++ backend/src/main/java/enums/ResourceType.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum ResourceType {
+	
+	LESSON_ONLY, LESSON_TASK, FORUM, EXTERNAL_LINK;
+	
+}
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -19,3 +19,7 @@ INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
 
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Olá',NOW(),false,'http://localhost:8080/',1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://c.pxhere.com/images/e7/04/ae65a1f30d7b6c505ad4eb7781bf-1449493.jpg!d', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'https://c.pxhere.com/images/e7/04/ae65a1f30d7b6c505ad4eb7781bf-1449493.jpg!d', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para as turmas', 3, 'https://c.pxhere.com/images/e7/04/ae65a1f30d7b6c505ad4eb7781bf-1449493.jpg!d', 0, 1);
